766700b2ad51f05b31e72a3d66c928d44d3f63ea
kwphil <121273949+kwphil@users.noreply.github.com>
2024-03-27 09:57:18 -0600

    kernel32.c

---
+++ boot/kernel32.c
@@ -0,0 +1,23 @@
+/*
+ *  Kernel32
+ *  
+ *  What this accomplishes:
+ *  Does system-wide checks to make sure everything is good
+ *  Checks if caps lock is pressed. If so, run kernel32 w/ debug ON by default
+ */
+
+unsigned char SHA256[256]; // uint8_t check for SHA
+
+void kernel32( void ) {
+    // * Shouldn't load Kernel64 just yet
+    // * Do a check of memory of SHA256
+    // * Will be assigned during compilation
+
+    // TODO: Create a SHA256 encoder
+
+    // TODO: Post the message to quit switch64 for 3 seconds
+
+    // TODO: Check if input = ESCAPE_CODE
+    unsigned char scancode = port_byte_in(0x60);
+    if()
+}   
