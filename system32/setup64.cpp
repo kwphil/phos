a4021aff4379a317dbfc5c5bbab872ee36941da9
kwphil <121273949+kwphil@users.noreply.github.com>
2024-03-28 09:02:33 -0600

    setup64

--- system32/setup64.cpp
+++ system32/setup64.cpp
@@ -1,6 +1,12 @@
+extern void switch_64( void ); /* Defined in switch64.asm */
+
 void setup64( void ) {
     // TODO: Create a sha512 check. I know that we did this with 256, but do it again.
+    shaCheck.check(sha);
 
-    // TODO: Set cr0 to move to 64bit
-     
+    // * Here we are switching to 64 bit
+    switch_64();
 }
+
+SHA512 shaCheck; /* Class dedicated to SHA512 */
+char sha[512];
