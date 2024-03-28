715f7ebbb8bbc58f6bd38f2666356df7e992604f
kwphil <121273949+kwphil@users.noreply.github.com>
2024-03-28 09:16:50 -0600

    Update kernel32.c

--- system32/kernel32.c
+++ system32/kernel32.c
@@ -1,3 +1,6 @@
+#include "../include/keyboard32.h"
+#include "../include/print32.h"
+
 /*
  *  Kernel32
  *  
@@ -17,6 +20,7 @@ void kernel32( void ) {
     // TODO: Create a SHA256 encoder
 
     // TODO: Post the message to quit switch64 for 3 seconds
-
+    puts("Press 'i' to escape 32bit boot!");
     // TODO: Check if input = ESCAPE_CODE
+    checkC('i')
 }   
